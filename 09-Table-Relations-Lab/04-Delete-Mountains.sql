c3ac82e6024da66062fc500f5d33022c1e046c2c
don <stivnnkolov@gmail.com>
2022-08-18 20:52:34 +0300

    Added solutions for Table relations lab

---
+++ 09-Table-Relations-Lab/04-Delete-Mountains.sql
@@ -0,0 +1,19 @@
+DROP TABLE `peaks`;
+DROP TABLE `mountains`;
+
+CREATE TABLE `mountains`
+(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20)
+);
+
+CREATE TABLE `peaks`
+(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains`
+FOREIGN KEY (`mountain_id`)
+REFERENCES `mountains`(`id`)
+ON DELETE CASCADE
+);
